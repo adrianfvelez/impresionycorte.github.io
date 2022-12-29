c3aa77311c438303ae6bce7d6ea1fab08f4cd581
Adrián Vélez <adrianf_velez@ciencias.unam.mx>
2022-12-29 01:47:36 -0600

    imágenes en assets

---
+++ _nuxt/static/1672299966/impresion-gran-formato/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fimpresionycorte.github.io\u002F_nuxt\u002Fstatic\u002F1672299966",layout:"default",error:a,serverRendered:true,routePath:"\u002Fimpresion-gran-formato",config:{_app:{basePath:"\u002Fimpresionycorte.github.io\u002F",assetsPath:"\u002Fimpresionycorte.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1672299966/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fcorte-a-contorno","\u002F","\u002Fimpresion-digital","\u002Ffoil","\u002Flaminado","\u002Fimpresion-gran-formato","\u002Fpromocionales","\u002Fubicacion"]})
